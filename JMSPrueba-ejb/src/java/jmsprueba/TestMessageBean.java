796afbf55174ebe533976a7ef22dbfd0a5c1e6f0
thatta <karentatianaalmansajimenez@gmail.com>
2019-11-17 22:52:10 -0500

    terminado

---
+++ JMSPrueba-ejb/src/java/jmsprueba/TestMessageBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmsprueba;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+/**
+ * 
+ * @author USER
+ */
+@MessageDriven(mappedName = "jms/QueueTest", activationConfig = { //se encarga de procesar los mensajes que llegan de la cola
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+/**
+ * 
+ */
+public class TestMessageBean implements MessageListener {
+    /**
+     * 
+     */
+    public TestMessageBean() {
+    }
+   /**
+    * 
+    * @param message 
+    */
+    @Override
+    public void onMessage(Message message) {
+        try { 
+            System.out.println("El mensaje enviado es:"+ ((TextMessage)message).getText()); 
+        } catch (JMSException ex) {
+          System.out.println("Error en el mensaje");
+        }  
+    }
+}
